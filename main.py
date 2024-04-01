8011ca6edebad18c3300fcec4d3117a9c78270e9
nennneko5787 <nennneko5787@gmail.com>
2024-04-01 11:03:55 +0900

    await

--- main.py
+++ main.py
@@ -44,11 +44,16 @@ async def create_db_pool():
 async def create_db_pool():
 	await app.db_pool.close()
 
-async def sjis(response):
-	data = await response.get_data(raw=False)
-	encoded_data = data.decode('utf8', errors='ignore').encode('shift_jis', errors='ignore')
-	response.set_data(encoded_data)
+async def sjis(dat):
+	response = await dat
+	# response.dataを取得するためにawaitを使用しない
+	data = response.get_data()
+	# 新しいContent-Typeヘッダーを追加
 	response.headers.add('Content-Type', 'text/plain; charset=shift_jis')
+	# データをUTF-8からShift-JISに変換し、エラーが発生した場合は無視する
+	encoded_data = data.decode('utf8', errors='ignore').encode('shift_jis', errors='ignore')
+	# レスポンスのデータを変更
+	response.set_data(encoded_data)
 	return response
 
 # Quartのページ類
